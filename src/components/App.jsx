043e87c1337fcfa08048068f9eed23827f48658b
WiktorSi <vitek2@poczta.onet.pl>
2023-05-15 22:27:21 +0200

    RE HO-1-2 Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import {Profile} from './Profile/Profile'
 import user from '../data/user.json'
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json'
 
 export const App = () => {
   return (
@@ -13,13 +15,15 @@ export const App = () => {
         color: '#010101'
       }}
     >
-    <Profile
+       <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-    /> 
+      /> 
+      
+      <Statistics title="Upload stats" stats={data} />
       
     </div>
   );
